6d1e87abd3b6f24aac78952d750fdb973c5232d9
Wagass25 <agalikhenry@gmail.com>
2023-04-04 09:10:56 +0100

    All_tasks :sparkles:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an empty Rectangle class using the pass keyword"""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
